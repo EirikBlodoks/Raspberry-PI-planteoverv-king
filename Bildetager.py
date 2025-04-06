275b7c8addcfa3aeeb94a19ba244e366798e6080
EirikBlodoks <93592010+EirikBlodoks@users.noreply.github.com>
2025-04-06 17:28:45 +0200

    Add files via upload

--- Bildetager.py
+++ Bildetager.py
@@ -1,32 +1,46 @@
 from picamera2 import Picamera2
-from time import sleep
 import os
+import sys
+import psutil
+
+def drep_existing_instances():
+    current_pid = os.getpid()  
+    script_name = os.path.basename(__file__)  #Henta koden for å slette eksisterende instances fra ett standardprosjekt på en RPI-blogg
+#Dette på grunn av en bug som oppsto hvis RPI kræsja, som gjorde at den ikke kjørte picam.close() nederst i koden 
+    for process in psutil.process_iter(['pid', 'name', 'cmdline']): 
+        try:
+            if process.info['pid'] != current_pid and process.info['cmdline']:
+                # Check if the script name appears in the command line arguments
+                if script_name in " ".join(process.info['cmdline']):
+                    print(f"Stopping existing instance: PID {process.info['pid']}")
+                    os.kill(process.info['pid'], 9)  # Force kill the process
+        except (psutil.NoSuchProcess, psutil.AccessDenied, psutil.ZombieProcess):
+            continue  
+
+drep_existing_instances()
+
+#Alt etter dette er derimot skrevet for egen maskin
 
 base_dir = "/home/eirik/Desktop/Planteprosjekt/regular pics"
-restart_counter_file = "/home/eirik/Desktop//Planteprosjekt/reboot_counter.txt"
+pic_counter_file = "/home/eirik/Desktop/Planteprosjekt/pic_counter.txt"
 
 os.makedirs(base_dir, exist_ok=True)
 
-if os.path.exists(restart_counter_file):
-    with open(restart_counter_file, "r") as f:
-        restart_count = int(f.read().strip())
+if os.path.exists(pic_counter_file):
+    with open(pic_counter_file, "r") as f:
+        try:
+            pic_count = int(f.read().strip())
+        except ValueError:
+            pic_count = 0  # Hvis filen er tom eller inneholder feil verdi
 else:
-    restart_count = 0
+    pic_count = 0
     
-restart_count+=1
-with open(restart_counter_file, "w") as f:
-    f.write(str(restart_count))
-
-session_dir = os.path.join(base_dir, f"session_{restart_count}")
-os.makedirs(session_dir, exist_ok=True)
-pic_count=0
-
 picam2 = Picamera2()
-while True:
-	filename= f"{session_dir}/plant{pic_count}.jpg"
-	secondFilename = f"{base_dir}/temp/plant{pic_count}.jpg"
-	picam2.start_and_capture_file(filename)
-	picam2.start_and_capture_file(secondFilename)
-	pic_count+=1
-	sleep(900)
+filename= f"/home/eirik/Desktop/Planteprosjekt/regular pics/New/plant{pic_count}.jpg"
+secondFilename = f"/home/eirik/Desktop/Planteprosjekt/regular pics/temp/plant{pic_count}.jpg"
+picam2.start_and_capture_file(filename)
+picam2.start_and_capture_file(secondFilename)
+pic_count+=1
+with open(pic_counter_file, "w") as f:
+    f.write(str(pic_count))
 picam2.close()
